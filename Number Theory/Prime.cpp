0ad1b99f5223127dfe6c61c6deca1335ca138937
Dabuliule <614151250@qq.com>
2023-02-05 15:17:26 +0800

    埃氏筛法/欧拉筛法

--- Number Theory/Prime.cpp
+++ Number Theory/Prime.cpp
@@ -32,38 +32,38 @@ int main()
 // 欧拉筛法
 // 每个合数都是被他最小的质因数筛掉
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-const int N = 1e6 + 10;
-bool vit[N];
-int primes[N];
+// const int N = 1e6 + 10;
+// bool vit[N];
+// int primes[N];
 
-int main()
-{
-    int n;
-    cin >> n;
+// int main()
+// {
+//     int n;
+//     cin >> n;
 
-    int cnt = 0;
-    for (int i = 2; i <= n; ++i)
-    {
-        if (!vit[i])
-            primes[cnt++] = i;
-        for (int j = 0; primes[j] <= n / i; ++j)
-        {
-            // 每次都要使用最小的质因数筛掉 i * primes[j]
-            vit[i * primes[j]] = true;
-            if (i % primes[j] == 0)
-                // i % primes[j] == 0
-                // 换言之，i 之前被 primes[j] 筛过了
-                // 由于 primes 里面质数是从小到大的
-                // 所以 i 乘上其他的质数的结果一定会被 primes[j] 的倍数筛掉
-                // 就不需要在这里先筛一次，所以这里直接 break
-                break;
-        }
-    }
+//     int cnt = 0;
+//     for (int i = 2; i <= n; ++i)
+//     {
+//         if (!vit[i])
+//             primes[cnt++] = i;
+//         for (int j = 0; primes[j] <= n / i; ++j)
+//         {
+//             // 每次都要使用最小的质因数筛掉 i * primes[j]
+//             vit[i * primes[j]] = true;
+//             if (i % primes[j] == 0)
+//                 // i % primes[j] == 0
+//                 // 换言之，i 之前被 primes[j] 筛过了
+//                 // 由于 primes 里面质数是从小到大的
+//                 // 所以 i 乘上其他的质数的结果一定会被 primes[j] 的倍数筛掉
+//                 // 就不需要在这里先筛一次，所以这里直接 break
+//                 break;
+//         }
+//     }
 
-    cout << cnt << endl;
+//     cout << cnt << endl;
 
-    return 0;
-}
+//     return 0;
+// }
