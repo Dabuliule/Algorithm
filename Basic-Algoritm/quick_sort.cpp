3737ab4445bbbe18b3c20ac59a297eb32a5d2f6f
Dabuliule <614151250@qq.com>
2023-03-03 20:04:03 +0800

    添加注释

--- Basic-Algoritm/quick_sort.cpp
+++ Basic-Algoritm/quick_sort.cpp
@@ -5,34 +5,43 @@ using namespace std;
 const int N = 100010, INF = 1e5;
 int a[N];
 
-void quickSort(int l, int r){
-    if(l >= r) return;
-    
+void quickSort(int l, int r)
+{
+    if (l >= r)
+        return;
+
     int i = l, x = a[l + r >> 1], p = l - 1, q = r + 1;
-    while(i < q){
-        while(i < q && a[i] > x) swap(a[i], a[--q]);
-        while(i < q && a[i] == x) ++i;
-        while(i < q && a[i] < x) swap(a[i++], a[++p]);
+    while (i < q)
+    {
+        while (i < q && a[i] > x)
+            swap(a[i], a[--q]);
+        while (i < q && a[i] == x)
+            ++i;
+        while (i < q && a[i] < x)
+            swap(a[i++], a[++p]);
     }
-    
+
     quickSort(l, p);
     quickSort(q, r);
 }
 
-int main(){
+int main()
+{
     int n;
     scanf("%d", &n);
-    
-    for(int i = 1; i <= n; ++i){
+
+    for (int i = 1; i <= n; ++i)
+    {
         scanf("%d", &a[i]);
     }
-    
+
     quickSort(1, n);
-    
-    for(int i = 1; i <= n; ++i){
+
+    for (int i = 1; i <= n; ++i)
+    {
         printf("%d ", a[i]);
     }
-    
+
     system("pause");
     return 0;
 }
